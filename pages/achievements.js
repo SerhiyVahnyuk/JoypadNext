7acfe08071c032fe424b715aa72adfe2c021956a
Sergey Vahnyuk <emeraldmaster35@gmail.com>
2024-06-01 15:13:01 +0300

    1.06.2024 Next

---
+++ pages/achievements.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default function Achievements() {
+  return (
+    <main>
+        
+        <div>
+            <h1>JOYPAD</h1>
+            <button>Home</button>
+            <button>Chat</button>
+        </div>
+        <div>
+            <div>
+                <div class="row row-cols-5">
+
+                </div>
+            </div>
+        </div>
+            
+    </main>
+  );
+}
