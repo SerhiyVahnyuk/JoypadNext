53ad7697ba7289507a3f58635f33644d4c6c68a7
Sergey Vahnyuk <emeraldmaster35@gmail.com>
2024-06-07 22:06:15 +0300

    Commit 07.06

--- pages/profile.js
+++ pages/profile.js
@@ -30,13 +30,42 @@ export default function Home() {
         }
     })
     return (
-        <div>
-            {info && 
-                <div>
-                    {info.name}
-                    <Image loader={()=>info.avatar} src={info.avatar} width={300} height={300}/>
+        <div className="main_page">
+
+        
+        <header>
+            <div className="headerLogo">
+              <Image loader={() => `http://localhost:8000/image/logo.png`} src={"http://localhost:8000/image/logo.png"} alt="Logo" width={47} height={47} />
+              <h3 className="logo">Joypad</h3>
+            </div>
+
+            <div className="headerButtons">
+
+              <button className="transperentButton">Home</button>
+              <button className="transperentButton">Chat</button>
+
+            </div>
+        </header>
+            <div className="main_profile">
+                <div className="double_main">
+                    <div className="menu_bar">
+                        <div className="object-top">
+                            <Image loader={() => `http://localhost:8000/image/strelka_left.png`} src={"http://localhost:8000/image/strelka_left.png"} alt="left" width={36} height={36} />
+                            <h3>Back</h3>
+                        </div>
+                    </div>
+                    <div className="menu_profile">
+                    </div>
                 </div>
-            }
+            </div>
         </div>
     )
 }
+
+
+                {/* {info && 
+                    <div>
+                        {info.name}
+                        <Image loader={()=>info.avatar} src={info.avatar} width={300} height={300}/>
+                    </div>
+                } */}
